d1c175e4691a3e8dd09eabcf9289f036566dc832
Sara D'Arce <saradarce@gmail.com>
2024-02-11 22:20:40 -0800

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import { useState, useEffect } from "react";
-import Form from "./components/Form";
-import MovieList from "./components/MovieList";
 import "./App.css";
+import MovieList from "./components/MovieList";
+import Form from "./components/Form";
 
 export default function App() {
+  const apiKey = "aabdfa24";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async (searchTerm) => {
+    try {
+      // Make fetch request and store the response
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      // Parse JSON response into a JavaScript object
+      const data = await response.json();
+      // Set the Movie state to the received data
+      setMovie(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Deadpool");
+  }, []);
+
+  // We pass the getMovie function as a prop called moviesearch
   return (
-    <>
-      <h1>SBA 320H React Web App</h1>
-    </>
+    <div className="App">
+      <Form searchMovies={getMovie} />
+      <MovieList movie={movie} />
+    </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,48 @@
 import { useRef } from "react";
 
-export default function Form() {}
+export default function Form() {
+  const inputRef = useRef(null);
+
+  function handleClick() {
+    console.dir(inputRef.current);
+    inputRef.current.focus();
+  }
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "50px",
+        }}
+      >
+        <input
+          ref={inputRef}
+          style={{
+            width: "250px",
+            padding: "5px",
+          }}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "10px",
+        }}
+      >
+        <button
+          onClick={handleClick}
+          style={{
+            width: "250px",
+            padding: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Search Movies
+        </button>
+      </div>
+    </>
+  );
+}
